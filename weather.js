52f931a2bc95b6deb1bd4c70d8be2123aac884c6
Justin Gibbs <justin.gibbs59@gmail.com>
2021-08-27 16:06:32 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ const searchButton = document.querySelector(".searchButton");
 async function searchWeather() {
     mainContainer.innerHTML = "";
     const search = document.querySelector('.searchCity').value;
-    const grabWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=43b8bdb66497ded16b243f1c51e00090`);
+    const grabWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=43b8bdb66497ded16b243f1c51e00090`);
     const jsonWeather = await grabWeather.json();
     const weatherDiv = document.createElement("div");
     weatherDiv.className = "weather";
